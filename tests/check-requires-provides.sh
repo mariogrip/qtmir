f4695e4d48d2fe7c9170c0d48566f732ff4eb3f9
Marius Gripsgard <marius@ubports.com>
2020-09-22 05:24:49 +0200

    [debian] Clean up old Replaces/Conflicts and fix gles and checks

--- tests/check-requires-provides.sh
+++ tests/check-requires-provides.sh
@@ -6,11 +6,11 @@ if [ $? -ne 0 ]; then
     exit 0
 fi
 
-requires=`grep unity-shell-application= CMakeLists.txt | sed -E 's/.*=(.*)\)/\1/'`
-provides=`grep unity-application-impl- debian/control | sed -E 's/.*unity-application-impl-(.*),/\1/'`
+requires=`grep lomiri-shell-application= CMakeLists.txt | sed -E 's/.*=(.*)\)/\1/'`
+provides=`grep lomiri-application-impl- debian/control | sed -E 's/.*lomiri-application-impl-(.*),/\1/'`
 if [ "$requires" -eq "$provides" ]; then
     exit 0
 else
-    echo "Error: We require unity-shell-application=$requires but provide unity-application-impl-$provides"
+    echo "Error: We require lomiri-shell-application=$requires but provide lomiri-application-impl-$provides"
     exit 1
 fi
